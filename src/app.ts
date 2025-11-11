dd044eff5773f48b14001db46017b09edefe95f8
Devidas-G <Dev.gaikwad222@gmail.com>
2025-11-11 18:32:54 +0530

    commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+import routes from "./routes";
+
+const app = express();
+app.use(express.json());
+app.use("/api", routes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript API!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
