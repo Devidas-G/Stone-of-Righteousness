debcce28e29dbc93db1a3c9e0993ed9c42c40446
Devidas-G <Dev.gaikwad222@gmail.com>
2026-01-14 15:09:40 +0530

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { getExample } from "../controllers/example.controller";
 import healthRouter from "./health.route";
-import itemRouter from "./item.route";
 import wishRouter from "../features/wishes/routes";
 import mediaRouter from "../features/media/routes";
 import authRouter from "../features/auth/routes";
@@ -10,7 +9,6 @@ const router = Router();
 
 router.get("/example", getExample);
 router.use("/health", healthRouter);
-router.use("/items", itemRouter);
 router.use("/wishes", wishRouter);
 router.use("/media", mediaRouter);
 router.use("/auth", authRouter);
